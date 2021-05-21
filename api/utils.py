8a4b5d97014b12c5d2dbc2225b7341eb65d30d5f
Vladislav Uhwatov <vlad.uhwatov@yandex.ru>
2021-05-21 22:47:40 +0200

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -11,7 +11,7 @@ def validate(function):
     def wrapper(*args, **kwargs):
         return JsonResponse(access_denied, status=403, json_dumps_params={'indent': 4}) \
             if not (any(list(map(lambda data: data == settings.API_TOKEN,
-                                 [args[-1].headers.get('Access-Token', None),
+                                 [args[-1].headers.get('access-token', None),
                                   kwargs.get('token', None)])))) else function(*args, **kwargs)
     return wrapper
 
