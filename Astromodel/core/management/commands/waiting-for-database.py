1a0894b5185ca24694f17cfe593778ac4d2cfdf7
Vladislav Uhwatov <vlad.uhwatov@yandex.ru>
2021-12-08 11:10:58 +0300

    Update waiting-for-database.py

--- Astromodel/core/management/commands/waiting-for-database.py
+++ Astromodel/core/management/commands/waiting-for-database.py
@@ -2,9 +2,11 @@ from time import sleep
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management import BaseCommand
+from api.models import Query
  
 class Command(BaseCommand): 
-    def handle(self, *args, **kwargs):
-        while not locals().get('database_connection', None):
-            try: database_connection = connections['default']
-            except OperationalError: sleep(5)
+	def handle(self, *args, **kwargs):
+		while not locals().get('database_connection', None):
+        	try: database_connection = connections['default']
+         	except OperationalError: sleep(5)
+		else: Query.objects.all().delete()
